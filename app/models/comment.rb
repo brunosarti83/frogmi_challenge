86482d1585a0d6f64abde9bfe61102ed51d1cebd
brunosarti83 <brunosarti.bs@gmail.com>
2024-04-13 19:30:57 -0300

    comments are created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :feature
+
+  validates :body, presence: true
+  validates :body, length: { minimum: 1, maximum: 300 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :features
+  get "/api/features", to: "features#index"
+  post "/api/features/:feature_id/comments", to: "comments#create"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
